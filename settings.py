de0d2303242c808458ffaf2d91b9514af175e9d2
Eng_Omar_Mohamed <42245792+omarmohammed271@users.noreply.github.com>
2026-05-24 21:50:35 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-starrise-dev-key-ch
 
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,178.62.66.109").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
